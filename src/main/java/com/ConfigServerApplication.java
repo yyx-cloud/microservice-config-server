9ed6e3c4a58c218c52b9def2fd3a714a6d161482
yyx19941129 <18600094425@163.com>
2019-01-14 17:34:05 +0800

    config融合eureka

--- src/main/java/com/ConfigServerApplication.java
+++ src/main/java/com/ConfigServerApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 /**
@@ -13,6 +14,7 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 @EnableConfigServer
+@EnableDiscoveryClient
 public class ConfigServerApplication {
     public static void main(String[] args) {
         SpringApplication.run(ConfigServerApplication.class);
